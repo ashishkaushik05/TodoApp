4f7efd46f98e092641271a7d25492367e7302db0
Ashish Kaushik <ashish._.kaushik@outlook.com>
2024-06-05 22:55:22 +0530

    trying

--- app/api/todo/[id]/route.js
+++ app/api/todo/[id]/route.js
@@ -1,6 +1,8 @@
 import Todo from "@models/todo";
 import { connectToDB } from "@utils/database";
 
+export const maxDuration = 300;
+
 export const PATCH = async (request, { params }) => {
     const { id } = params;
     const { completed } = await request.json();
